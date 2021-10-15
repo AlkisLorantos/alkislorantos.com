5215989b4a2154dba82f7166e9c4d68c7e524c94
Alkis <alkis.lorantos@icloud.com>
2021-10-15 19:34:55 +0300

    Added some files

---
+++ pages/tweets.js
@@ -0,0 +1,52 @@
+import Container from "@/components/Container";
+import Tweet from "@/components/Tweet";
+import { getTweets } from "@/lib/twitter";
+
+export default function Tweets({ tweets }) {
+  return (
+    <Container
+      title="Tweets – Alkis Lorantos"
+      description="A collection of tweets that make me laugh, make me think and make me learn."
+    >
+      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+          Tweets
+        </h1>
+        <p className="text-gray-600 dark:text-gray-400 mb-4">
+          Collection of my personal favourite tweets. 🕊
+        </p>
+        <p className="text-gray-600 dark:text-gray-400 mb-4">
+          Also, not to forget, built using the Twitter API V2 (Super easy, super
+          fun). 🕊
+        </p>
+        {tweets.map((tweet) => (
+          <Tweet key={tweet.id} {...tweet} />
+        ))}
+      </div>
+    </Container>
+  );
+}
+
+export async function getStaticProps() {
+  const tweets = await getTweets([
+    "1371103308425928708",
+    "826528907381739520",
+    "1308509070140162048",
+    "997895977179721728",
+    "1341090253864542208",
+    "1026872652290379776",
+    "1346113149112619016",
+    "1340107217970683906",
+    "992629481578745856",
+    "989142253468708864",
+    "1368058884837928970",
+    "1341962177272537089",
+    "1342869937841266688",
+    "1116362674319908864",
+    "1360610614490501126",
+    "1371103308425928708",
+  ]);
+
+  return { props: { tweets } };
+}
+
