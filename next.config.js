6065488062984817959c07d2645da76ca5f6fc2e
Alkis <alkis.lorantos@icloud.com>
2021-10-16 15:52:18 +0300

    Next Config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "i.scdn.co", // Spotify Album Art
+      "pbs.twimg.com", // Twitter Profile Picture
+      "api.microlink.io", // Microlink Image Preview
+    ],
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (isServer) {
+      require("./scripts/generate-sitemap");
+    }
+
+    return config;
+  },
+};
