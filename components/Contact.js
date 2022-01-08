74db19e50c9ab78fd1587ac61007e08a35ae15b7
Alkis <alkis.lorantos@icloud.com>
2022-01-08 15:39:42 +0200

    added whats app

--- components/Contact.js
+++ components/Contact.js
@@ -60,7 +60,7 @@ export default function Contact() {
         Drop your message and let's discuss about your project.
       </p>
       <a
-        href="https://wa.me/919587738861?text=I want to work on a project with you"
+        href="https://wa.me/306949034464?text=Hey i just came from your website"
         className="mt-4 px-4 text-center w-full font-bold h-8 bg-green-400 dark:bg-green-400 text-gray-900 py-1 dark:text-gray-900 rounded"
         type="submit"
       >
